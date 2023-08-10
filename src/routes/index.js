fba6baf34379023692cf83d884c33b9516f21bb7
Vule <vu.le@edugix.com>
2023-08-11 00:01:33 +0700

    Push project

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const UserRouter = require("./UserRouter");
+const OrderRouter = require("./OrderRouter");
+const ProductRouter = require("./ProductRouter");
+const PaymemtRouter = require("./PaymemtRouter");
+const routes = (app) => {
+  app.use("/api/user", UserRouter);
+  app.use("/api/product", ProductRouter);
+  app.use("/api/order", OrderRouter);
+  app.use("/api/payment", PaymemtRouter);
+};
+module.exports = routes;
