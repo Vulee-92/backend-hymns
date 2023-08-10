17c70b67134ada2ec61b5bc23b0fe48a6bdb4632
Vule <vu.le@edugix.com>
2023-08-11 00:24:27 +0700

    update

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/ProductController');
-const { authMiddleWare } = require('../middleware/authMiddleware');
+const productController = require("../controllers/ProductController");
+const { authMiddleWare } = require("../middleware/authMiddleware");
 
-
-router.post('/create', productController.createProduct)
-router.put('/update/:id', productController.updateProduct)
-router.get('/get-details/:id',  productController.getDetailsProduct)
-router.delete('/delete-product/:id',authMiddleWare, productController.deleteProduct)
-router.get('/get-all', productController.getAllProduct)
-router.post('/delete-many',authMiddleWare, productController.deleteMany)
-router.get('/get-all-type', productController.getAllType)
+router.post("/create", productController.createProduct);
+router.put("/update/:id", productController.updateProduct);
+router.get("/get-details/:id", productController.getDetailsProduct);
+router.delete(
+  "/delete-product/:id",
+  authMiddleWare,
+  productController.deleteProduct
+);
+router.get("/get-all", productController.getAllProduct);
+router.post("/delete-many", authMiddleWare, productController.deleteMany);
+router.get("/get-all-type", productController.getAllType);
 module.exports = router;
